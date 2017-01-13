529d01a84d6f19d317ce83a33a81b20c93033618
Dan DeGreef <daniel_degreef@condenast.com>
2017-01-12 18:33:50 -0600

    fix typo

--- RCSwitch.sh
+++ RCSwitch.sh
@@ -4,39 +4,39 @@ switchNumber="${1}"
 
 case ${switchNumber} in 
    "1_on")  
-      code = 4199731
+      code=4199731
       ;;  
    "1_off")  
-      code = 4199740
+      code=4199740
       ;; 
    "2_on")  
-      code = 4199875
+      code=4199875
       ;; 
    "2_off")  
-      code = 4199884
+      code=4199884
       ;; 
    "3_on")  
-      code = 4200195
+      code=4200195
       ;; 
    "3_off")  
-      code = 4200204
+      code=4200204
       ;; 
    "4_on")  
-      code = 4201731
+      code=4201731
       ;; 
    "4_off")  
-      code = 4201740
+      code=4201740
       ;; 
    "5_on")  
-      code = 4207875
+      code=4207875
       ;; 
    "5_off")  
-      code = 4207884
+      code=4207884
       ;; 
 esac 
 
-protocol = 1
-delay = 182
+protocol=1
+delay=182
 
 #run the codesend command 3 times to ensure the switch doesn't miss the command
 sudo 433Utils/RPi_utils/codesend "$code" "$protocol" "$delay"
