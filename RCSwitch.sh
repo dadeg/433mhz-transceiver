0f8ce3bc0381aaa0b67d2d76e3b11f70c1ee7915
Dan DeGreef <daniel_degreef@condenast.com>
2017-02-06 21:02:30 -0600

    add new devices

--- RCSwitch.sh
+++ RCSwitch.sh
@@ -3,14 +3,11 @@
 # this script accepts multiple arguments, usage: ./RCSwitch.sh 1_on 2_on 3_on
 # to add a new device, simply add it to the switch statement below and give it a name to reference in the args.
 
-# Here are the defaults for the protocol and delay, if you add different devices that require a different delay, you may need to customize this script
-protocol=1
-delay=182
-
 codesToSend=()
 
 for codeName in "$@"
 do
+	# numbers are [code] [protocol] [delay].
 	case ${codeName} in 
 	   "0330_1_on") 
 	      codesToSend+=("4199731 1 182")
@@ -19,29 +16,93 @@ do
 	      codesToSend+=("4199740 1 182")
 	      ;; 
 	   "0330_2_on")  
-	      codesToSend+=("4199875")
+	      codesToSend+=("4199875 1 182")
 	      ;; 
 	   "0330_2_off")  
-	      codesToSend+=("4199884")
+	      codesToSend+=("4199884 1 182")
 	      ;; 
 	   "0330_3_on")  
-	      codesToSend+=("4200195")
+	      codesToSend+=("4200195 1 182")
 	      ;; 
 	   "0330_3_off")  
-	      codesToSend+=("4200204")
+	      codesToSend+=("4200204 1 182")
 	      ;; 
 	   "0330_4_on")  
-	      codesToSend+=("4201731")
+	      codesToSend+=("4201731 1 182")
 	      ;; 
 	   "0330_4_off")  
-	      codesToSend+=("4201740")
+	      codesToSend+=("4201740 1 182")
 	      ;; 
 	   "0330_5_on")  
-	      codesToSend+=("4207875")
+	      codesToSend+=("4207875 1 182")
 	      ;; 
 	   "0330_5_off")  
-	      codesToSend+=("4207884")
+	      codesToSend+=("4207884 1 182")
 	      ;; 
+	   
+	   
+	   "0316_1_on") 
+	      codesToSend+=("5575987 1 182")
+	      ;;  
+	   "0316_1_off")  
+	      codesToSend+=("5575996 1 182")
+	      ;; 
+	   "0316_2_on")  
+	      codesToSend+=("5576131 1 182")
+	      ;; 
+	   "0316_2_off")  
+	      codesToSend+=("5576140 1 182")
+	      ;; 
+	   "0316_3_on")  
+	      codesToSend+=("5576451 1 182")
+	      ;; 
+	   "0316_3_off")  
+	      codesToSend+=("5576460 1 182")
+	      ;; 
+	   "0316_4_on")  
+	      codesToSend+=("5577987 1 182")
+	      ;; 
+	   "0316_4_off")  
+	      codesToSend+=("5577996 1 182")
+	      ;; 
+	   "0316_5_on")  
+	      codesToSend+=("5584131 1 182")
+	      ;; 
+	   "0316_5_off")  
+	      codesToSend+=("5584140 1 182")
+	      ;;
+	  
+	  
+	   "0320_1_on") 
+	      codesToSend+=("5330227 1 179")
+	      ;;  
+	   "0320_1_off")  
+	      codesToSend+=("5313852 1 179")
+	      ;; 
+	   "0320_2_on")  
+	      codesToSend+=("5313987 1 179")
+	      ;; 
+	   "0320_2_off")  
+	      codesToSend+=("5313996 1 179")
+	      ;; 
+	   "0320_3_on")  
+	      codesToSend+=("5314307 1 179")
+	      ;; 
+	   "0320_3_off")  
+	      codesToSend+=("5314316 1 179")
+	      ;; 
+	   "0320_4_on")  
+	      codesToSend+=("5315843 1 179")
+	      ;; 
+	   "0320_4_off")  
+	      codesToSend+=("5315852 1 179")
+	      ;; 
+	   "0320_5_on")  
+	      codesToSend+=("5321987 1 179")
+	      ;; 
+	   "0320_5_off")  
+	      codesToSend+=("5321996 1 179")
+	      ;;  
 	esac 
 done
 
